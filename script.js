94470176047c86fe2e9112b8c76ec1c4e9f8c6f2
Eric Michalsen <eric.michalsen@gmail.com>
2016-12-02 14:40:17 -0600

    2

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
+
+// Set Variables
 var system = require('system');
 var os     = system.os;
 var args   = system.args;
 var name   = args[1];
 var url    = args[2];
+var device = args[3];
 
+// Create Page
 var page   = require('webpage').create();
 
+// Set that UserAgent
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
+
+// Third argument set viewport width
+  if (device == 'p') {
+    var size = 400;
+    var device_name = 'phone';
+  }
+  if (device == 't') {
+    var size = 760
+    var device_name = 'tablet';
+  }
+  if (device == 'd') {
+    var size = 1200
+    var device_name = 'desktop';
+   }
+
+// Set the viewportSize
+  page.viewportSize = {
+    width: size,
+    height: 800
+  };
+
+// Open that bad boy up
 page.open(url, function(status) {
+    // Oops, something wrong happened
     if (status !== 'success') {
          console.log('Unable to access ' + url);
          console.log(status);
      } else {
 
-      console.log(name + ' ' + url);
+      //console.log(name + ' ' + url);
 
       var title = page.evaluate(function() {
         return document.title;
@@ -26,10 +54,14 @@ page.open(url, function(status) {
 
       //var content = page.content;
 
-      console.log('Title: ' + title);
-      console.log('Description: ' + description);
-      //console.log(content);
-      page.render('images/' + name + '.png');
+       if (typeof device === 'undefined') {
+          console.log('Title: ' + title);
+          console.log('Description: ' + description);
+          //console.log(content);
+       }
+         else {
+          page.render('images/' + name + '_' + device_name + '.png');
+        }
 
     }
   phantom.exit();
