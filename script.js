7a06d2b4931e78a3c82d870c97123997f4805eeb
Eric Michalsen <eric.michalsen@gmail.com>
2016-12-02 13:31:13 -0600

    first

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var url    = args[2];
 
 var page   = require('webpage').create();
 
-//page.settings.userAgent = 'SN Browser';
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
 page.open(url, function(status) {
