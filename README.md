9e9a0c573acec81f1517c48e4a19a316187b8ad1
Eric Michalsen <eric.michalsen@gmail.com>
2017-01-31 09:40:09 -0600

    jan31

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ phantomjs [name] [full url] [p|t|d]
 [p|t|d] phone / tablet / desktop rendering
 
 if not supplied only site content will be returned
+
+
+
+phantomjs script.js fleetridge http://www.fleetridge.com p|t|d
--- script.js
+++ script.js
@@ -42,7 +42,7 @@ page.open(url, function(status) {
          console.log(status);
      } else {
 
-      //console.log(name + ' ' + url);
+
 
       // Get that title
       var title = page.evaluate(function() {
@@ -50,26 +50,51 @@ page.open(url, function(status) {
       });
 
       // Get the desciption
-      var description = page.evaluate(function() {
-        return document.querySelector("meta[name='description']").content;
+      // var description = page.evaluate(function() {
+      //   return document.querySelector("meta[name='description']").content;
+      // });
+
+      // Get the form
+      form_id = page.evaluate(function() {
+        return document.forms[0].id;
       });
 
-      // Find specific class.
-      // TESTING ERRORS HERE
-      var pnt = page.evaluate(function() {
-        return document.querySelectorAll(".pnt");
-      });
+      // Get the form Elements
+      // form_elements = page.evaluate(function() {
+      //   return document.getElementById(document.forms[0].id).elements;
+      // });
 
+      inputs = page.evaluate(function() {
+        return document.getElementsByTagName("input");
+      });
 
       //var content = page.content;
 
       // If device is not set from the arguments, print
       // the title, description
       if (typeof device === 'undefined') {
-          console.log('Title: ' + title);
-          console.log('Description: ' + description);
-          //console.log(pnt);
-          //console.log(content);
+          // console.log('Title: ' + title);
+          // console.log('Description: ' + description);
+          // console.log(inputs.length);
+
+          //console.log(inputs[1].tagName + inputs[1].getAttribute('name'));
+          // console.log(document.getElementById("webform-client-form-6").elements);
+          // console.log(document.forms[0].id);
+           // console.log(inputs[0]);
+           for (var rows in inputs[0][type]) {
+             console.log(rows);
+           }
+            // for (var i=0; i < inputs.length; i++)
+            // {
+            //    // if (inputs[i].getAttribute('type') == 'text')
+            //    // {
+            //        //console.log(inputs[i].tagName+inputs[i].getAttribute('name'));
+            //        console.log(inputs[i]);
+            //    // }
+            // }
+
+
+
       }
         else {
           page.render('images/' + name + '_' + device_name + '.png');
